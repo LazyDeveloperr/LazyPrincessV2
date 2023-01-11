d13597d77ac8118d873dd91d6ba7a68442ebf952
Santu Ghosh <69458659+me-santu@users.noreply.github.com>
2023-01-11 23:52:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/me-santu/LazyPrincessV2 /LazyPrincessV2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincessV2
